8cb5afb411885392ea85fcc1c23f64acb1606255
ruotianliu <43013253+ruotianliu@users.noreply.github.com>
2018-12-05 17:52:38 -0500

    Update and rename search_label.py to mysql_search_label.py

--- mysql_search_label.py
+++ mysql_search_label.py
@@ -3,11 +3,10 @@ import sys
 import os
 
 
-key = input("please input keyword: ")
+key = input("please input label: ")
 db = pymysql.connect("localhost", "root", "lrtbest2018", "test")
 myCursor = db.cursor()
 
-#choose from table at label1
 sql = "SELECT * FROM tweetsInfomation \
            WHERE labels LIKE '%s'" % (key)
 myCursor.execute(sql)
